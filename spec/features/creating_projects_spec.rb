b0dbb5aa6b8ae52b6448052ae5257909a9e77cc4
HimanshuM <himanshu.malpande@gmail.com>
2016-04-20 19:21:35 +0530

    Add title functionality for show page

--- spec/features/creating_projects_spec.rb
+++ spec/features/creating_projects_spec.rb
@@ -11,6 +11,11 @@ feature 'Creating Projects' do
 		click_button 'Create project'
 
 		expect(page).to have_content('Project has been created.')
+
+		project = Project.where(name: "nano").first
+		expect(page.current_url).to eql(project_url(project))
+		title = "nano - Ticketee"
+		expect(page).to have_title(title)
     end
 end
 
