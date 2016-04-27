95060dd3188958327c25f541e1bbc26d3d0db29c
HimanshuM <himanshu.malpande@gmail.com>
2016-04-27 15:17:07 +0530

    Implement displaying a list of relevant tickets. You can see them on projects, and view a particular ticket.

---
+++ spec/features/viewing_tickets_spec.rb
@@ -0,0 +1,29 @@
+require "rails_helper"
+
+feature "Viewing tickets" do
+	before do
+		nano = FactoryGirl.create(:project, name: "nano")
+
+		FactoryGirl.create :ticket, project: nano, title: "Make it shiny", description: "Gradients! Starbursts! Oh my!"
+
+		internet_explorer = FactoryGirl.create :project, name: "Internet Explorer"
+
+		FactoryGirl.create :ticket, project: internet_explorer, title: "Standards compliance", description: "Isn't a joke"
+
+		visit "/"
+	end
+
+	scenario "Viewing tickets for a given project" do
+		click_link "nano"
+
+		expect(page).to have_content "Make it shiny"
+		expect(page).to_not have_content "Standards compliance"
+
+		click_link "Make it shiny"
+		within "#ticket h2" do
+			expect(page).to have_content "Make it shiny"
+		end
+
+		expect(page).to have_content "Gradients! Starbursts! Oh my!"
+	end
+end
